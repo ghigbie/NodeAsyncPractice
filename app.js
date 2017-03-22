2438610fb5dd5e6fa27faeb55188e2cd0e7a9501
George Higbie <gehigbie@gmail.com>
2017-03-22 16:42:05 -0700

    remove duplicate app.js file

--- app.js
+++
@@ -1,9 +0,0 @@
-const request = require("request");
-
-//request will take two arguments  //the first argument is an options object and the secodn is a callback that gets called when json comes back from api
-request({
-	url: "https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia",
-	json: true
-}, (error, response, body) => {  
-	console.log(body);
-});

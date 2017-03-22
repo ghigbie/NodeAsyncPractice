f645d7443ce7baefa5698b63fb1f4bf6d8623fd0
George Higbie <gehigbie@gmail.com>
2017-03-22 15:27:54 -0700

    add call fucntion to test async

--- weather-app/playground/asyn-basics.js
+++
@@ -1,3 +0,0 @@
-console.log("Starting App");
-
-console.log("Finishing Up");
---
+++ weather-app/playground/async-basics.js
@@ -0,0 +1,7 @@
+console.log("Starting App");
+
+setTimeOut(() => {
+	console.log("Inside of callback");
+}, 2000);
+
+console.log("Finishing Up");

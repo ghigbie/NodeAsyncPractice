90aa2835bb2be993c5477528c0dd18e2deb9c59f
George Higbie <gehigbie@gmail.com>
2017-03-22 17:42:02 -0700

    add directions to print formatted address

--- weather-app/app.js
+++ weather-app/app.js
@@ -5,5 +5,5 @@ request({
 	url: "https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia",
 	json: true
 }, (error, response, body) => {  
-	console.log(JSON.stringify(body, undefined, 2));
+	console.log(`Address: ${body.results[0].formatted_address}`);
 });

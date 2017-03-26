8124ccba81b93d477d2033b3a7547adf49ee77b9
George Higbie <gehigbie@gmail.com>
2017-03-26 11:35:09 -0700

    revive app.js

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,46 +1,26 @@
-// const yargs = require("yargs");
+const yargs = require("yargs");
 
-// const geocode = require("./geocode/geocode");
+const geocode = require("./geocode/geocode");
 
-// const argv = yargs
-// 	.options({
-// 		a: {
-// 			demand: true,
-// 			alias: "address",
-// 			describe: "Address that is needed to fetch weather",
-// 			string: true
-// 		}
-// 	})
-// 	.help()
-// 	.alias("help", "h")
-// 	.argv;
-
-// geocode.geocodeAddress(argv.address, (errorMessage, results) => {
-// 	if(errorMessage){
-// 		console.log(errorMessage);
-// 	}else{
-// 		console.log(JSON.stringify(results, undefined, 2));
-// 	}
-// 
-
-const request = require("request");
-
-var apiKey = "9f6325a874ba4e46242d3e5e3c349a27";
-var lat = "37.8267";
-var lng = "-122.4233";
-
-var ulrDarkSky = `https://api.darksky.net/forecast/${apiKey}/${lat},${lng}`;
-
-request({
-		url: ulrDarkSky,
-		json: true
-	},(error, response, body) => {
-		if(!error && response.statusCode == "200"){
-			console.log(`******** ${body.currently.temperature} *********`);
-		}else{
-			console.log("======Unable to fetch weather=======");
+const argv = yargs
+	.options({
+		a: {
+			demand: true,
+			alias: "address",
+			describe: "Address that is needed to fetch weather",
+			string: true
 		}
-	});
+	})
+	.help()
+	.alias("help", "h")
+	.argv;
+
+geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+	if(errorMessage){
+		console.log(errorMessage);
+	}else{
+		console.log(JSON.stringify(results, undefined, 2));
+	}
 
 
 

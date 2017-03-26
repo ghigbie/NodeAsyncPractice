fb89c4b80eef922f07e4e87f3ad0d242f86017e5
George Higbie <gehigbie@gmail.com>
2017-03-26 11:15:29 -0700

    add strings to make print reading easier

--- weather-app/app.js
+++ weather-app/app.js
@@ -36,9 +36,9 @@ request({
 		json: true
 	},(error, response, body) => {
 		if(!error && response.statusCode == "200"){
-			console.log(body.currently.temperature);
+			console.log(`******** ${body.currently.temperature} *********`);
 		}else{
-			console.log("Unable to fetch weather");
+			console.log("======Unable to fetch weather=======");
 		}
 	});
 

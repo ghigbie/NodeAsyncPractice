175fc09e587044eb9bbb179a78b71a465e8980bd
George Higbie <gehigbie@gmail.com>
2017-03-26 10:51:39 -0700

    use request to connect to dark sky

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,26 +1,48 @@
-const yargs = require("yargs");
+// const yargs = require("yargs");
 
-const geocode = require("./geocode/geocode");
+// const geocode = require("./geocode/geocode");
 
-const argv = yargs
-	.options({
-		a: {
-			demand: true,
-			alias: "address",
-			describe: "Address that is needed to fetch weather",
-			string: true
+// const argv = yargs
+// 	.options({
+// 		a: {
+// 			demand: true,
+// 			alias: "address",
+// 			describe: "Address that is needed to fetch weather",
+// 			string: true
+// 		}
+// 	})
+// 	.help()
+// 	.alias("help", "h")
+// 	.argv;
+
+// geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+// 	if(errorMessage){
+// 		console.log(errorMessage);
+// 	}else{
+// 		console.log(JSON.stringify(results, undefined, 2));
+// 	}
+// 
+
+const request = require("request");
+
+var apiKey = "9f6325a874ba4e46242d3e5e3c349a27";
+var lat = "37.8267";
+var lng = "-122.4233";
+
+var ulrDarkSky = `https://api.darksky.net/forecast/${apiKey}/${lat},${lng}`;
+
+request({
+		url: ulrDarkSky,
+		JSON: true
+	},(error, response, body)){
+		if(error){
+			console.log(error)
+		}else if(response === null){
+			console.log("No response");
+		}else if(body){
+			console.log(body.currently.temperature);
 		}
-	})
-	.help()
-	.alias("help", "h")
-	.argv;
+	};
 
-geocode.geocodeAddress(argv.address, (errorMessage, results) => {
-	if(errorMessage){
-		console.log(errorMessage);
-	}else{
-		console.log(JSON.stringify(results, undefined, 2));
-	}
-});
-
+	
 
